bda7db82e7126bb2913daaeadd4acfa91c521cb3
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-13 10:55:33 -0500

    Delete cloaker.js

--- cloaker.js
+++
@@ -1,9 +0,0 @@
-document.title = "Home - Google Drive";
-
-let favicon = document.createElement("link");
-
-favicon.rel = "icon";
-favicon.type = "image/png";
-favicon.href = "/favicon.png";
-
-document.head.appendChild(favicon);

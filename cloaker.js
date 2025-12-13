5d0aa437c36c3c95359f139aba8ba8feeb6be8de
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-13 10:54:35 -0500

    Add favicon and update document title in cloaker.js

---
+++ cloaker.js
@@ -0,0 +1,9 @@
+document.title = "Home - Google Drive";
+
+let favicon = document.createElement("link");
+
+favicon.rel = "icon";
+favicon.type = "image/png";
+favicon.href = "/favicon.png";
+
+document.head.appendChild(favicon);

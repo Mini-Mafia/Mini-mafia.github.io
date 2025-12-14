e6f4bc9404d233c59671fec163adb3ea8f28a477
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-14 14:41:21 -0500

    Fix data element reference in onclick handler

--- abc.js
+++ abc.js
@@ -1,7 +1,7 @@
 window.onload = () => {
  
 
-  data.onclick = () => {
+  document.getElementById("data").onclick = () => {
     window.location.href = `data:text/html, <html> <body> <iframe src="https://mini-mafia.github.io/g"></iframe> <style>html,body{margin:0}iframe{width:100%;height:100dvh}</style> </body> </html>`;
   };
 };

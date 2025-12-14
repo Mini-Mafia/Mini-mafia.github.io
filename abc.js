ece109befee21b2280676ad7f9e4390b54a02ed4
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-14 14:32:29 -0500

    Update iframe handling in abc.js

--- abc.js
+++ abc.js
@@ -5,4 +5,8 @@ window.onload = () => {
     bru.document.write(`<html><body><iframe src="https://mini-mafia.github.io/g"></iframe><style>iframe{width:100%;height:100dvh;border:none}body{margin:0}</style></body></html>`);
     bru.document.close();
   };
+
+  data.onclick = () => {
+    window.location.href = `data:text/html, <html> <body> <iframe src="https://mini-mafia.github.io/g"></iframe> <style>html,body{margin:0}iframe{width:100%;height:100dvh}</style> </body> </html>`;
+  };
 };

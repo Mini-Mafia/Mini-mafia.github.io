23ce94869d140e65a0b414482e92a6773a21cb85
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-14 13:57:29 -0500

    Open new window with specified URL and target

--- abc.js
+++ abc.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   ab.onclick = () => {
-    let bru = window.open();
+    let bru = window.open("about:blank", "_blank");
     if (!bru) alert("about blank cloak failed");
     bru.document.write(`<html><body><iframe src="https://mini-mafia.github.io/g"></iframe><style>iframe{width:100%;height:100dvh;border:none}body{margin:0}</style></body></html>`)
   };

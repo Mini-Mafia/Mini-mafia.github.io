719262d3467e59ecbc5f20ac0b440eb664acef42
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-14 14:23:02 -0500

    Fix window.open parameters and ensure document close

--- abc.js
+++ abc.js
@@ -1,7 +1,8 @@
 window.onload = () => {
   ab.onclick = () => {
-    let bru = window.open("about:blank", "_blank");
+    let bru = window.open("", "_blank");
     if (!bru) alert("about blank cloak failed");
-    bru.document.write(`<html><body><iframe src="https://mini-mafia.github.io/g"></iframe><style>iframe{width:100%;height:100dvh;border:none}body{margin:0}</style></body></html>`)
+    bru.document.write(`<html><body><iframe src="https://mini-mafia.github.io/g"></iframe><style>iframe{width:100%;height:100dvh;border:none}body{margin:0}</style></body></html>`);
+    bru.document.close();
   };
 };

228d47f46a5b0bbcfef980be80b0168587dc6a24
secretaayu-sys <secretaayu@gmail.com>
2025-12-14 13:32:27 -0500

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  ab.onclick = () => {
+    let bru = window.open();
+    if (!bru) alert("about blank cloak failed");
+    bru.document.write(`<html><body><iframe src="https://mini-mafia.github.io/g"></iframe><style>iframe{width:100%;height:100dvh;border:none}body{margin:0}</style></body></html>`)
+  };
+};

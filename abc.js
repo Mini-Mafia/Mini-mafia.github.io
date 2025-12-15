56e5b72a07744076bf125513a6661ab3ffec6c28
secretaayu-sys <secretaayu@gmail.com>
2025-12-15 17:36:13 -0500

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,7 +1,1 @@
-window.onload = () => {
- 
-
-  document.getElementById("data").onclick = () => {
-    window.location.href = `data:text/html, <html> <body> <iframe src="https://mini-mafia.github.io/g"></iframe> <style>html,body{margin:0}iframe{width:100%;height:100dvh}</style> </body> </html>`;
-  };
-};
+<script /index.js="https://cdn.jsdelivr.net/gh/FogNetwork/ABC/index.js"></script>

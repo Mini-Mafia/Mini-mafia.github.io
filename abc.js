93f1ba523c37f2d9fc8ab676cf032ba2e3af6675
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-25 20:40:51 -0500

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,25 +1,27 @@
-let cloak_btn = document.querySelector("button");
-
-let content = `<!DOCTYPE html>
-<!-- Please speed I need this my mom is kinda homeless -->
-<html>
-<body>
-<iframe src="https://mini-mafia.github.io/" allow="fullscreen"></iframe>
-<style>
-iframe {
-  height: 100dvh;
-  width: 100%;
-  border: none;
-}
-
-body {
-  margin: 0;
-}
-</style>
-</body>
-</html>`;
-
-const htmlBlob = new Blob([content], { type: "text/html" });
-let url = URL.createObjectURL(htmlBlob);
-
-link.href = url;
+window.onload = () => {
+  let cloak_btn = document.querySelector("button");
+  
+  let content = `<!DOCTYPE html>
+  <!-- Please speed I need this my mom is kinda homeless -->
+  <html>
+  <body>
+  <iframe src="https://mini-mafia.github.io/" allow="fullscreen"></iframe>
+  <style>
+  iframe {
+    height: 100dvh;
+    width: 100%;
+    border: none;
+  }
+  
+  body {
+    margin: 0;
+  }
+  </style>
+  </body>
+  </html>`;
+  
+  const htmlBlob = new Blob([content], { type: "text/html" });
+  let url = URL.createObjectURL(htmlBlob);
+  
+  link.href = url;
+};

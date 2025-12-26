2c24827d5708cd3f8b86ee08eaaf006ae27f9676
Felix Schwiebert <fschwieb@protonmail.com>
2025-12-25 19:53:48 -0500

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,1 +1,21 @@
-<script abc.js="https://cdn.jsdelivr.net/gh/FogNetwork/ABC/index.js"></script>
+let cloak_btn = document.querySelector("button");
+let content = `<!DOCTYPE html>
+<!-- Please speed I need this my mom is kinda homeless -->
+<html>
+<body>
+<iframe src="https://mini-mafia.github.io/" allow="fullscreen"></iframe>
+<style>
+iframe {
+  height: 100dvh;
+  width: 100%;
+}
+
+body {
+  margin: 0;
+}
+</style>
+</body>
+</html>`;
+const htmlBlob = new Blob([content], { type: "text/html" });
+let url = URL.createObjectURL(htmlBlob);
+window.open(url);
